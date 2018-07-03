4d10a74b58363fafef0fa0e36d4ec1b5b4258fac
Emma Rivera <erive011@ucr.edu>
2018-07-02 20:16:49 -0700

    Added Tweet amd User Models

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,29 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Tweet {
+
+    // list attributes
+    public String body;
+    public long uid;
+    public User user;
+    public String createdAt;
+
+    // function to deserialize data from the JSON
+    public static Tweet fromJSON(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+
+        // extract data from the JSON
+        tweet.body = jsonObject.getString("text");
+        tweet.uid = jsonObject.getLong("id");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+        return tweet;
+    }
+
+
+
+
+}
