5e786187294b72c376695d8b20a487c9ad46e2dc
Emma Rivera <erive011@ucr.edu>
2018-07-03 17:38:49 -0700

    Added Profile Image to Recycler View

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ import java.util.List;
 public class TweetAdapter extends  RecyclerView.Adapter<TweetAdapter.ViewHolder> {
 
     private List<Tweet> mTweets;
+    Context context;
 
     // pass in tweets array into constructor
     public TweetAdapter(ArrayList<Tweet> tweets) {
@@ -27,7 +29,7 @@ public class TweetAdapter extends  RecyclerView.Adapter<TweetAdapter.ViewHolder>
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
+        context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
         View tweetView = inflater.inflate(R.layout.item_tweet, parent, false);
@@ -44,6 +46,8 @@ public class TweetAdapter extends  RecyclerView.Adapter<TweetAdapter.ViewHolder>
         //populate the views at this position
         holder.tvUserName.setText(tweet.user.name);
         holder.tvBody.setText(tweet.body);
+
+        Glide.with(context).load(tweet.user.profileImageUrl).into(holder.ivProfileImage);
     }
 
     @Override
